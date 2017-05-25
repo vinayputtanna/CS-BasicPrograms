cda4e95042edc6a6ccaf69ba767b907745a8c1b8
Vinay Puttanna <vinayp@outlook.com>
2017-05-24 18:48:19 -0700

    Initial Commit

--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BasicPrograms
 {
@@ -10,6 +6,8 @@ namespace BasicPrograms
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome back to the world of .NET");
         }
     }
 }
